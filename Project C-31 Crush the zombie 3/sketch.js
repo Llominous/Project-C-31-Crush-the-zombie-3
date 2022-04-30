584fe61054a4331cad35e7ce5b8f0cb1aa3e6371
Llominous <96136466+Llominous@users.noreply.github.com>
2022-04-30 16:18:06 +0530

    Add files via upload

--- Project C-31 Crush the zombie 3/sketch.js
+++ Project C-31 Crush the zombie 3/sketch.js
@@ -97,19 +97,19 @@ function draw() {
       collided = true;
     }*/
 
-    /*if (distance <= 50) {
+    if (distance <= 50) {
       zombie.velocityX = 0;
       Matter.Body.setVelocity(stone.body, { x: 10, y: -10 });
       zombie.changeImage("sad");
       collided = true;
-    }*/
+    }
 
-    if (distance <= 50) {
+    /*if (distance <= 50) {
       zombie.velocityX = 0;
       Matter.Body.Velocity(stone.body, { x: 10, y: -10 });
       zombie.changeImage("sad");
       collided = true;
-    }
+    }*/
 
   }
 
